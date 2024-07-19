d6c443f882d5dbefa21f44f912ec495cb242d7d5
TEO <alfredo.ssm@gmail.com>
2024-07-19 16:59:04 -0400

    Se apunta a mock por caida de api

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -5,9 +5,61 @@ import Modal from './Modal';
 const CardContainer = () => {
     const [anime, setAnime] = useState([]);
     const getAnime = () => {
-        fetch('https://api-anime-app-production.up.railway.app/anime/list')
-            .then((response) => { return response.json() })
-            .then((data) => { setAnime(data) });
+        // fetch('https://api-anime-app-production.up.railway.app/anime/list')
+        //     .then((response) => { return response.json() })
+        //     .then((data) => { setAnime(data) });
+
+        const data = [
+            {
+                _id: '1',
+                name: 'Haikyuu!!',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868860/app-anime/anime/haikyuu_ru1jzu.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '2',
+                name: 'One Piece',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868861/app-anime/anime/one_piece_aiqnc6.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '3',
+                name: 'Fate/Zero',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868860/app-anime/anime/fate_zero_b3lgta.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '4',
+                name: 'Dragon Ball Z',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868860/app-anime/anime/dragon_ball_z_unmbhm.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '5',
+                name: 'Jujustu Kaisen',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868861/app-anime/anime/jujutsu_kaisen_uzf2rh.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '6',
+                name: 'Attack on Titan',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868860/app-anime/anime/attack_on_titan_q3uoof.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '7',
+                name: 'Death Note',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868860/app-anime/anime/death_note_za62gg.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+            {
+                _id: '8',
+                name: 'My Hero Academia',
+                image: 'https://res.cloudinary.com/teoisnotdead/image/upload/v1665868861/app-anime/anime/my_hero_academia_qzfzre.jpg',
+                url: 'https://www.youtube.com/watch?v=V8KTuiJeBH8',
+            },
+        ]
+        setAnime(data);
     }
 
     useEffect(() => {
